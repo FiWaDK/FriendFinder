0b991a806c24d288bca2acbcc999aac241cf3eb1
FiWaDK <filipwarning@gmail.com>
2025-06-22 18:56:00 +0200

    Add initial React frontend with Vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <main className="app">
+      <h1>FriendFinder</h1>
+      <p>Find nye venner baseret på dine interesser og værdier.</p>
+      <button>Kom i gang</button>
+    </main>
+  )
+}
+
+export default App
